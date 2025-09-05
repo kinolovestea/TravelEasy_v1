5d3ec7757572cad2af544c0286541ff3f504cc6a
Yu <katex1218@gmail.com>
2025-09-05 18:05:11 +1000

    Initial Commit

---
+++ TravelEasy/TravelEasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelEasyApp.swift
+//  TravelEasy
+//
+//  Created by Yu on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelEasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
